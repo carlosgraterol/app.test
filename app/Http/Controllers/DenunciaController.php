0db23dbe8f3417d2b62b019b8a4120e7b4e34532
Carlos Castle <20089565castillocarlos@gmail.com>
2020-09-12 16:38:26 -0300

    Actualización de Controlador Denuncia

--- app/Http/Controllers/DenunciaController.php
+++ app/Http/Controllers/DenunciaController.php
@@ -3,43 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Http\Requests\DenunciaRequest;
+use App\Models\Denuncia;
+use App\Models\Delito;
 
 class DenunciaController extends Controller
 {
-    public function formdenuncia()
-    {
+    public function index(){
 
-        return view('denuncia.denuncia');
+    	$delitos = Delito::all();
+    	return view('denuncia.denuncia', compact('delitos'));
+
     }
 
-    public function create()
-    {
-        //
-    }
+    public function storedenuncia(DenunciaRequest $request){
 
-    public function store(Request $request)
-    {
-        //
-    }
+    	if ($request->hasFile('documento')) {
+            $file = $request->file('documento');
+            $documento = time().'.'.$file->getClientOriginalExtension();
+            $file->move(public_path().'/upload/', $documento);
+        }else{
+            $documento = '';
+        }
 
-    public function show($id)
-    {
-        //
-    }
+        $denuncia = Denuncia::create([
+        	'anonima'         => $request->input('anonima'),
+            'nombre'          => $request->input('nombre'),
+            'rut'             => $request->input('rut'),
+            'email'           => $request->input('email'),
+            'delito_id'       => $request->input('delito_id'),
+            'fecha'           => $request->input('fecha'),
+            'identifipersona' => $request->input('identifipersona'),
+            'descripcion'     => $request->input('descripcion'),
+            'conocimiento'    => $request->input('conocimiento'),
+            'lugar'           => $request->input('lugar'),
+            'otrolugar'       => $request->input('otrolugar'),
+            'documento'       => $documento,
+        ]);
 
-    public function edit($id)
-    {
-        //
-    }
-
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    public function destroy($id)
-    {
-        //
+        if ($denuncia) {
+            return redirect()->route('home')->with('status','¡Su denuncia ha sido enviada correctamente!');
+        }else{
+            return redirect()->route('perfil.estudios')->with('statuswarning','Su denuncia no pudo ser enviada debido a un error interno. Por favor intente de nuevo.');
+        }
     }
 }
